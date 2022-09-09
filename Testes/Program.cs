4cc819a86814bb87f513e67441c5b6da790eed49
Gabrel Rodrigues <gabriel02aguiar@gmail.com>
2022-09-09 10:41:34 -0300

    first commit

--- Testes/Program.cs
+++ Testes/Program.cs
@@ -8,6 +8,7 @@ namespace Testes
         {
             Console.WriteLine("Testando o Git Hub");
             Console.WriteLine("Adicionar apenas a linha nova");
+            Console.WriteLine("Adicionando outra linha como teste");
         }
     }
 }
