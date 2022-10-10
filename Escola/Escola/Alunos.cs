c6aeddd765504e065d1383fa93237ca4bbd71a79
Gabrel Rodrigues <gabriel02aguiar@gmail.com>
2022-10-10 16:37:50 -0300

    first commit

---
+++ Escola/Escola/Alunos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola
+{
+     class Alunos
+     {
+        private int Matricula;
+        private string Nome;
+        private double N1;
+        private double N2;
+        private double Media;
+        private string Status;
+
+        public Alunos(int Matricula, string Nome, double N1, double N2, double Media, string Status)
+        {
+            this.Matricula = Matricula;
+            this.Nome = Nome;
+            this.N1 = N1;
+            this.N2 = N2;
+            this.Media = Media;
+            this.Status = Status;
+        }
+
+        public int getMatricula
+        {
+            get { return Matricula; }
+            set { Matricula = value; }
+        }
+        public string getNome
+        {
+            get { return Nome; }
+            set { Nome = value; }
+        }
+        public double getN1
+        {
+            get { return N1; }
+            set { N1 = value; }
+        }
+        public double getN2
+        {
+            get { return N2; }
+            set { N2 = value; }
+        }
+        public double getMedia
+        {
+            get { return Media; }
+            set { Media = value; }
+        }
+        public string getStatus
+        {
+            get { return Status; }
+            set { Status = value; }
+        }
+
+
+       
+     }
+}
---
+++ Escola/Escola/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace Escola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tbMatricula = new System.Windows.Forms.TextBox();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.tbN1 = new System.Windows.Forms.TextBox();
+            this.tbN2 = new System.Windows.Forms.TextBox();
+            this.tbMedia = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 286);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(625, 163);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tbMatricula
+            // 
+            this.tbMatricula.Location = new System.Drawing.Point(97, 37);
+            this.tbMatricula.Name = "tbMatricula";
+            this.tbMatricula.Size = new System.Drawing.Size(413, 20);
+            this.tbMatricula.TabIndex = 1;
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(97, 88);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(413, 20);
+            this.tbNome.TabIndex = 2;
+            // 
+            // tbN1
+            // 
+            this.tbN1.Location = new System.Drawing.Point(97, 135);
+            this.tbN1.Name = "tbN1";
+            this.tbN1.Size = new System.Drawing.Size(73, 20);
+            this.tbN1.TabIndex = 4;
+            // 
+            // tbN2
+            // 
+            this.tbN2.Location = new System.Drawing.Point(263, 135);
+            this.tbN2.Name = "tbN2";
+            this.tbN2.Size = new System.Drawing.Size(73, 20);
+            this.tbN2.TabIndex = 5;
+            // 
+            // tbMedia
+            // 
+            this.tbMedia.Location = new System.Drawing.Point(437, 135);
+            this.tbMedia.Name = "tbMedia";
+            this.tbMedia.Size = new System.Drawing.Size(73, 20);
+            this.tbMedia.TabIndex = 6;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(515, 33);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 7;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(515, 135);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(515, 85);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 9;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Matricula:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "N1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Nome:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(392, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Media:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "N2:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.tbMedia);
+            this.Controls.Add(this.tbN2);
+            this.Controls.Add(this.tbN1);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.tbMatricula);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox tbMatricula;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.TextBox tbN1;
+        private System.Windows.Forms.TextBox tbN2;
+        private System.Windows.Forms.TextBox tbMedia;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
